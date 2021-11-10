3407d4539eb93a70ef79ee98d51f4f496e8765e1
ldlywt <ldlywt@gmail.com>
2021-11-10 21:06:35 +0800

    Add String.getEmptyOrDefault

--- app/src/main/java/com/ldlywt/commoncode/ktx/UtilsKtx.kt
+++ app/src/main/java/com/ldlywt/commoncode/ktx/UtilsKtx.kt
@@ -3,6 +3,8 @@ package com.ldlywt.commoncode.ktx
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
 import android.os.Bundle
 import android.view.View
 import android.view.inputmethod.InputMethodManager
@@ -71,4 +73,18 @@ fun View.clickWithLimit(intervalMill: Int = 500, block: ((v: View?) -> Unit)) {
             }
         }
     })
+}
+
+fun Context.isConnectedNetwork(): Boolean = run {
+    val cm = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
+    val activeNetwork: NetworkInfo? = cm.activeNetworkInfo
+    activeNetwork?.isConnectedOrConnecting == true
+}
+
+inline fun <reified T> String.getEmptyOrDefault(default: () -> T): T {
+    return if (isNullOrEmpty() || this == "null") {
+        default()
+    } else {
+        this as T
+    }
 }
